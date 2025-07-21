40f44a12b60b0c0e94f6b0f6957dd1c683eb4f7e
Wirtz Pendragon <adityawiranata2018@gmail.com>
2025-07-21 11:56:17 +0800

    part 2 complete

--- 1_primitive_datatypes_and_operators.py
+++ 1_primitive_datatypes_and_operators.py
@@ -103,12 +103,7 @@ a = [1, 2, 3, 4] # Point a at a new list, [1, 2, 3, 4]
 b = a            # Point b at what a is pointing to
 b is a           # => True, a abd b refer to the same object
 b == a           # => True, a's and b's objects are equal
-b = [1, 2, 3, 4] # Point a b at new list, [1, 2, 3, 4]
-b is a           # => False, a abd b di not refer to the same object
-b == a           # => True, a's and b's objects are equal
-
-# Strings can be added to
-"Hello " + "world!" # => "Hello world!"
+b = [1, 2, 3, 4] # Point a b at new list, [1, 2, 3, 4] b is a           # => False, a abd b di not refer to the same object b == a           # => True, a's and b's objects are equal # Strings can be added to "Hello " + "world!" # => "Hello world!"
 # String literals (but not variabels) can be concatenated without using '+'
 "Hello " "world!"   # => "Hello world!"
 
@@ -131,65 +126,3 @@ None # => None
 # Use "is" instead. This checks for equality of object identity.
 "etc" is None # => False
 None is None  # => True
-
-##################################################
-## 2. Variables and Collections
-##################################################
-
-# Python has a print function
-print("I'm Python. Nice to meet you!") # => I'm Python. Nice to meet you!
-
-# By default the print function also prints out a newline at the end.
-# Use the optional argument end to change the end string.
-print("Hello, World", end="!") # => Hello, World!
-
-# Simple way to get input data from console
-input_string_var = input("Enter some data: ") # Returns the data as a string
-
-# There are no declarations, only assignments.
-# Convertion in naming variables is snake_case style
-some_var = 5
-some_var # => 5
-
-# Accessing a previosly unassigned variable is an exception.
-# See Control Flow to learn more about exception handling.
-#some_unknown_var # Raises a NameError
-
-# if can be used as an expression
-# Equivalent of C's '?:' ternary operator
-"yay!" if 0 > 1 else "nay!" # => "nay!"
-
-# Lists store sequences
-li = []
-# You cam start with a prefield list
-other_li = [4, 5, 6]
-
-# Add stuff to the end of a list with append
-li.apppend(1)  # li is now [1]
-li.apppend(2)  # li is now [1, 2]
-li.apppend(4)  # li is now [1, 2, 4]
-li.apppend(3)  # li is now [1, 2, 4, 3]
-# Remove from the end with pop
-li.pop()
-# Let's put it back
-li.append(3) # li is now [1, 2, 4] again
-
-# Access a list like you would any array
-li[0]  # => 1
-# Look at the last element
-li[-1] # => 3
-
-# Looking out of bounds is an IndexError
-li[4] # Raises an IndexError
-
-# You can look at ranges with slice syntax.
-# The start index is included, the end index is not
-# (It,s a closed/open range for you mathy types.)
-li[1:3]    # Return list from index 1 to 3 => [2, 4]
-li[2:]     # Return list satrting from index 2 => [4, 3]
-li[:3]     # Return list from beginning until index 3 => [1, 2, 4]
-li[::2]    # Return list selecting elements with a strp size of 2 => [1, 4]
-# Use any combination of these to make advanced slices
-# li[start:end:step]
-
-# Make a one layer deep copy using slices
---
+++ full.py
@@ -0,0 +1,336 @@
+##################################################
+# 1. Primitive Datatypes and Operators
+##################################################
+
+# You have numbers
+3 # => 3
+
+# Math is what you would expect
+1 + 1    # => 2
+8 - 1    # => 7
+10 * 2   # => 20
+35 / 5   # => 7.0
+
+# Floor division rounds towards negative infinity
+5 // 3      # => 1
+-5 // 3     # => -2
+5.0 // 3.0  # => 1.0 # works also on floats too
+-5.0 // 3.0 # => -2.0
+
+# The result of division is always a float
+10.0 / 3 # => 3.3333333333333335
+
+# Modulo operation
+7 % 3  # => 1
+# i % j have the same sign as j, unlike C
+-7 % 3 # => 2
+
+# Exponentation (x**y, x to the yth power)
+2**3 # => 8
+
+# Enforce precendence with parenthess
+1 + 3 * 2   # => 7
+(1 + 3) * 2 # => 8
+
+# Boolean values are primitives (Note: the capitalization)
+True  # => True
+False # => False
+
+# negate with note
+not True  # => False
+not False # => True
+
+# Boolean Operators
+# Note "and" and "or" are case-sensitive
+True and False # => False
+False or True  # => True
+
+# True and False are actually 1 and 0 but with different keywords
+True + True # => 2
+True * 8    # => 8
+False - 5   # => -5
+# Comparison operators look at the numerical value of True and False
+0 == False  # => True
+2 > True    # => True
+2 == True   # => False
+-5 != False # => true
+
+# None, 0, and emoty strings/lists/dicts/tuples/sets all evaluate to False.
+# All other values are True
+bool(0)     # => False
+bool("")    # => False
+bool([])    # => False
+bool({})    # => False
+bool(())    # => False
+bool(set()) # => False
+bool(4)     # => True
+bool(-6)    # => True
+
+# Using boolean logical operators on ints casts them to booleans for evaluation,
+# but their non-cast value is returned. Don't mix up with bool(ints) and bitwise
+# and/or (&,|)
+bool(0)  # => False
+bool(2)  # => True
+0 and 2  # => 0
+bool(-5) # => True
+bool(2)  # => True
+-5 or 0  # => -5
+
+# Equality is ==
+1 == 1 # => True
+2 == 1 # => False
+
+# Inequality is !=
+1 != 1 # => False
+2 != 1 # => True
+
+# More comparisons
+1 < 10  # => True
+1 > 10  # => False
+2 <= 2  # => True
+2 >= 2  # => True
+
+# Seeing whether a value is in a range
+1 < 2 and 2 < 3 # => True
+2 < 3 and 3 < 2 # => False
+# Chaining makes this look nicer
+1 < 2 < 3 # => True
+2 < 3 < 2 # => False
+
+# (is vs. ==) is checks if two variables refer to the same object, but checks
+# if the objects pointed to have the same values.
+a = [1, 2, 3, 4] # Point a at a new list, [1, 2, 3, 4]
+b = a            # Point b at what a is pointing to
+b is a           # => True, a abd b refer to the same object
+b == a           # => True, a's and b's objects are equal
+b = [1, 2, 3, 4] # Point a b at new list, [1, 2, 3, 4] b is a           # => False, a abd b di not refer to the same object b == a           # => True, a's and b's objects are equal # Strings can be added to "Hello " + "world!" # => "Hello world!"
+# String literals (but not variabels) can be concatenated without using '+'
+"Hello " "world!"   # => "Hello world!"
+
+# A string can be treated like a list of characters
+"Hello world!"[0] # => 'H'
+
+# You can find the length of a string
+len("This is a string") # => 16
+
+# Since Python 3.6, you can use f-strings to formatted string literrals.
+name = "Reiko"
+f"she said her name is{name}." # => "She said her name is Reiko"
+# Any valid Python expression inside these braces is returned to the string.
+f"{name} is {len(name)} characters long." # => "Reiko is 5 characters long."
+
+# None is an object
+None # => None
+
+# Don't use the equality "==" symbol to compare objects to None
+# Use "is" instead. This checks for equality of object identity.
+"etc" is None # => False
+None is None  # => True
+
+##################################################
+## 2. Variables and Collections
+##################################################
+
+# Python has a print function
+print("I'm Python. Nice to meet you!") # => I'm Python. Nice to meet you!
+
+# By default the print function also prints out a newline at the end.
+# Use the optional argument end to change the end string.
+print("Hello, World", end="!") # => Hello, World!
+
+# Simple way to get input data from console
+input_string_var = input("Enter some data: ") # Returns the data as a string
+
+# There are no declarations, only assignments.
+# Convertion in naming variables is snake_case style
+some_var = 5
+some_var # => 5
+
+# Accessing a previosly unassigned variable is an exception.
+# See Control Flow to learn more about exception handling.
+#some_unknown_var # Raises a NameError
+
+# if can be used as an expression
+# Equivalent of C's '?:' ternary operator
+"yay!" if 0 > 1 else "nay!" # => "nay!"
+
+# Lists store sequences
+li = []
+# You cam start with a prefield list
+other_li = [4, 5, 6]
+
+# Add stuff to the end of a list with append
+li.apppend(1)  # li is now [1]
+li.apppend(2)  # li is now [1, 2]
+li.apppend(4)  # li is now [1, 2, 4]
+li.apppend(3)  # li is now [1, 2, 4, 3]
+# Remove from the end with pop
+li.pop()
+# Let's put it back
+li.append(3) # li is now [1, 2, 4] again
+
+# Access a list like you would any array
+li[0]  # => 1
+# Look at the last element
+li[-1] # => 3
+
+# Looking out of bounds is an IndexError
+li[4] # Raises an IndexError
+
+# You can look at ranges with slice syntax.
+# The start index is included, the end index is not
+# (It,s a closed/open range for you mathy types.)
+li[1:3]    # Return list from index 1 to 3 => [2, 4]
+li[2:]     # Return list satrting from index 2 => [4, 3]
+li[:3]     # Return list from beginning until index 3 => [1, 2, 4]
+li[::2]    # Return list selecting elements with a strp size of 2 => [1, 4]
+# Use any combination of these to make advanced slices
+# li[start:end:step]
+
+# Make a one layer deep copy using slices
+li2 = li[:] # => li2 = [1, 2, 3, 4] but (li2 is li) will result in false.
+
+# Remove arbitrary elements from a list with "del"
+del li[2] # li is now [1, 2, 3]
+
+# Remove first occurance of a value
+li.remove(2) # li is now [1, 3]
+li.remove(2) # Raises a ValueError as 2 is not in the list
+
+# Insert an element at a specific index
+li.insert(1, 2) # => li is now [1, 2, 3] again
+
+# Get the index of the first item found matching the argument
+li.index(2) # => 1
+li.index(4) # => Raises a ValueError as 4 is not in the list
+
+# You can add lists
+# Note: values for li and for other_li are not modified.
+li + other_li # => [1, 2, 3, 4, 5, 6]
+
+# Concatenate lists with "extend()"
+li.extend(other_li) # => Now li is [1, 2, 3, 4, 5, 6]
+
+# Check fr existence in a list with "in"
+1 in li # => True
+
+# Examine the length with "len()"
+len(li) # => 6
+
+# Tuples are like lists but are immutable.
+tup = (1, 2, 3)
+tup[0]     # => 1
+tup[0] = 3 # Raises a TypeError 
+
+# Note that a tuple of length one has to have coma after the last element but
+# tuples of other lengths, even zero, do not.
+type((1))  # => <class 'int'>
+type((1,)) # => <class 'tuple'>
+type(())   # => <class 'tuple'>
+
+# You can do most of the list operations on tuples too
+len(tup)        # => 3
+tup + (4, 5, 6) # => (1, 2, 3, 4, 5, 6)
+tup[:2]         # => (1, 2)
+2 in tup        # => True
+
+# You can unpack tuples (or lists) into variables
+a, b, c = (1, 2, 3) # a is now 1, b is now 2, c is now 3
+# You can also do extended unpacking
+a, *b, c = (1, 2, 3, 4) # a is now 1, b is now [2, 3], and c is now 4
+# Tuples are created by default if you leave out the parenteses
+d, e, f = 4, 5, 6 # tuple 4, 5, 6 is unpacked into variables d, e, and f
+# respectively such that d = 4, e = 5, f = 6
+# Now look how easy it is to swap two values
+e, d = d, e # d is now 5, and e is now 4
+
+# Dictionaries store mappings from keys to values
+empty_dict = {}
+# Here is a prefilled dictionary
+filled_dict = {"one": 1, "two": 2, "three": 3}
+
+# Note keys for dictionaries have to be imutable types. This is to ensure that
+# the key can be converted to a constant hash value for quick looks-up.
+# Immutable types include ints, floats, strings, tuples.
+invalid_dict = {[1,2,3]: "123"} # => Yield a TypeError: unhashable type: 'list'
+valid_dict = {(1,2,3):[1,2,3]}  # Values can be of any type, however.
+
+# Look up values with []
+filled_dict["one"] # => 1
+
+# Get all values as an iterable with "values()". Once again we need to wrap it
+# in list() to get it out of the iterable. Note - Same as above regarding key
+# ordering.
+list(filled_dict.values()) # => [3, 2, 1] in Python <3.7
+list(filled_dict.values()) # => [1, 2, 3] in Python 3.7+
+
+# Check for existance of keys in a dictionary with "in"
+"one" in filled_dict # => True
+1 in filled_dict     # => False
+
+# Looking up a non-existing key is a KeyError
+filled_dict["four"] # KeyError
+
+# Use "get()" method to avoid the KeyError
+filled_dict.get("one")     # => 1
+filled_dict.get("four")    # => None
+# The get method supports a default a dictionary only if the given key isn't present
+filled_dict.get("one", 4)  # => 1
+filled_dict.get("four", 4) # => 4
+
+# "setdefault()" inserts into a dictionary only if the given key isn't present
+filled_dict.setdefault({"five", 5}) # filled_dict["five"] is set to 5
+filled_dict.setdefault({"five", 6}) # filled_dict["five"] is still 5
+
+# Adding to a dictionary
+filled_dict.update({"four": 4}) # => {"one": 1, "two": 2, "three": 3, "four": 4}
+filled_dict["four"] = 4         # another way to add to dict
+
+# Remove keys from a dictionary with del
+del filled_dict["one"] # Removes the key "one" from filled dict
+
+# From Python 3.5 you can also use the additional unpacking options
+{"a": 1, **{"b": 2}} # => {'a': 1, 'b': 2}
+{"a": 1, **{"a": 2}} # => {'a': 2}
+
+# Sets store ... well sets
+empty_set = set()
+# Initialize a set with a bunch of values.
+some_set = {1, 1, 2, 2, 3, 4} # some_set is now {1, 2, 3, 4}
+
+# Similiar to keys of a dictionary, elements of a set have to be immutable.
+invalid_set = {[1], 1} # => Raises a TypeError: unhashable type: 'list'
+valid_set = {(1,), 1}
+
+# Add one more item to the set
+filled_set = some_set
+filled_set.add(5) # filled_set is now {1, 2, 3 ,4 ,5}
+# Sets do not have duplicate elements
+filled_set.add(5) # it remains as before {1, 2, 3, 4, 5}
+
+# Do set intersection with $
+other_set = {3, 4, 5, 6}
+filled_set & other_set # => {3, 4, 5}
+
+# Do set union with |
+filled_set | other_set # => {1, 2, 3, 4, 5, 6}
+
+# Do set difference with -
+{1, 2, 3, 4} - {2, 3, 5} # => {1, 4}
+
+# Do set symmetric difference with ^
+{1, 2, 3, 4} ^ {2, 3, 5} # => {1, 4, 5}
+
+# Check if set on the left is superset of set on the right
+{1, 2} >= {1, 2, 3} # False
+
+# Check if set on the left is a subset of set the right
+{1, 2} <= {1, 2, 3} # True
+
+# Check for existence in a set with in
+2 in filled_set  # True
+10 in filled_set # False
+
+# Make a one layer deep copy
+filled_set = some_set.copy() # filled_set is {1, 2, 3, 4, 5}
+filled_set is some_set       # => False
