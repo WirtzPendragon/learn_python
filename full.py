96f8b26b77d467cb9a0192888dd60a7731a118bc
Wirtz Pendragon <adityawiranata2018@gmail.com>
2025-07-21 16:58:03 +0800

    part 5 & 6

--- full.py
+++ full.py
@@ -639,3 +639,172 @@ list(filter(lambda x: x > 5, [3, 4, 5, 6, 7])) # => [6, 7]
 # You can construct set and dict comprehensions as well.
 {x for x in "abcddeef" if x not in "abc"} # => {'d', 'e', 'f'}
 {x: x**2 for x in range(5)} # => {0: 0, 1: 1, 2: 4, 3: 9, 4: 16}
+
+
+##################################################
+## 5. Modules
+##################################################
+
+# You can import modules
+import math
+print(math.sqrt(16)) # => 4.0
+
+# You can get specified functions from a module
+from math import ceil, floor
+print(ceil(3.7))  # => 4
+print(floor(3.7)) # => 3
+
+# You can import all functions from a module.
+# Warning: this is not recommended
+from math import *
+
+# You can shorten module names
+import math as m
+math.sqrt(16) == m.sqrt(16) # => True
+
+# Python modules are just ordinary Python files. You
+# can write your own, and import them. The name of the
+# module is the same as the name of the file.
+
+# You can find out which functions and attributes
+# are definied in a module.
+import math
+dir(math)
+
+# If you have a Python script name math.py in the same
+# folder as your current script, the file math.py will
+# be loaded instead of the built-in Python module.
+# This happens because the local folder has priority
+# over Python's built-in libraries.
+
+##################################################
+## 6. Classes
+##################################################
+
+# We use the "class" statement to create a class
+class Human:
+
+    # A class attribute. It is shared by all instances of this class
+    species = "H. sapiens"
+
+    # Basic initializer, this is called when this class is instanted.
+    # Note that the double leading and trailing underscores denote objects
+    # or attributes that are used by Python but that live in user-controlled
+    # namespaces. Methods(or object or attributes) like: __int__, __str__,
+    # __repr__ etc. are called special methods (or sometimes called dunder)
+    # methods). You should not invent such names on your own.
+    def __init(self, name):
+        # Assign the argument to the instance's name attribute
+        self.name = name
+        
+        # Initialize property
+        self._age = 0 # the leading underscore indicates the "age" property is
+                      # intended to be used internally
+                      # do not rely on this to be enforced: it's a hint to other devs
+
+     # An instance method. All methods take "self" as the first argument
+    def say(self, msg):
+        print("{name}: {message}".format(name=self.name, message=msg))
+
+    # Another instance method
+    def sing(self):
+        return "yo... yo... microphone check... one two... one two..."
+    
+    # A class method is shared among all instances
+    # They are called with the calling class as the first argument
+    @classmethod
+    def get_species(cls):
+        return cls.species
+    
+    # A static method is called without a class or instance reference
+    @staticmethod
+    def grunt():
+        return "*grunt*"
+    
+    # A property is just like a getter.
+    # It turns the method age() into a read-only attributes of the same name.
+    # There's no need to write trivial getters and setters in Python, though.
+    @property
+    def age(self):
+        return self._age
+    
+    # This allows the property to be set
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    # This allows the property to be deleted
+    @age.deleter
+    def age(self):
+        del self._age
+
+# When a Python interpreter reads a source file it executes all its code.
+# This __name__ check makes sure this code block executed when this
+# module is the main program.
+if __name__ == "__main__":
+    # Instantiate a class
+    i = Human(name="Ian")
+    i.say("hi")             # "Ian: hi"
+    j = Human("Joel")
+    j.say("hello")          # "Joel: hello"
+    # i and j are instances of type Human; i.e, they are Human objects.
+
+    # Call our class method
+    i.say(i.get_species())      # "Ian: H. sapiens"
+    # Change the shared attribute
+    Human.species = "H. neanderthalensis"
+    i.say(i.get_species())      # => "Ian: H. neanderthalensis"
+    j.say(j.get_species())      # => "Joel: H. neanderthalensis"
+
+    # Call the static method
+    print(Human.grunt())        # => "*grunt*"
+
+    # Static methods can be called by instances too
+    print(i.grunt())            # => "*grunt*"
+
+    # Update the property for this instance
+    i.age = 42
+    # Get the property
+    i.say(i.age)                # => "Ian: 42"
+    j.say(j.age)                # => "Joel: 0"
+    # Delete the property
+    del i.age
+    # i.age                     # => this would raise an AttributeError
+
+
+##################################################    
+## 6.1 Inheritance
+##################################################
+
+# Inheritance allows new child classes to be defined that inherit methods and
+# variables from their parent class.
+
+# Using the Human class defined above as the base or parent class, we can
+# define a child class, Superhero, which inherits variables like "species",
+# "name", and "age", as well as methods, like "sing" and "grunt"
+# from the Human class, but can also have its own unique properties.
+
+# To take advantage of modularization by file you could place the classes above
+# in their own files, say, human.py
+
+# To import functions from other files use the following format
+# from "filename-without-extension" import "function-or-class"
+
+from human import Human
+
+# Specify the parent class(es) as parameters to the class definition
+class Superhero(Human):
+
+    # If the child class should inherit all of the parent's definitions without
+    # any modifications, you can just use the "pass" keyword (and nothing else)
+    # but in this case it is commented out allow for a unique child class:
+    # pass
+
+    # Child classes can override their parents' attributes
+    species = "Superhuman"
+
+    # Children automatically inherit their parent class's constructor including
+    # its arguments, but can also define additional arguments or definitions
+    # and override its methods such as the class constructor.
+    # adds the "superpower" and "movie" arguments:
+    
--- run.py
+++
@@ -1,6 +0,0 @@
-def add(x, y):
-    print("x is {} and y is {}".format(x, y))
-    return x + y # Return values with a return statement
-
-result = add(3, 5)
-print("Result is:", result)
