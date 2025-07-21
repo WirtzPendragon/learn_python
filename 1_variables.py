54efec664a85854ca864c0c4e389ab03a17ae89b
Wirtz Pendragon <adityawiranata2018@gmail.com>
2025-07-21 10:05:19 +0800

    first commit

---
+++ 1_variables.py
@@ -0,0 +1,51 @@
+# print start
+# print("Hello! My name is Aditya.")
+# print end
+
+# if condition start
+# x = 1
+# if x == 1:
+#     print("x is 1")
+# if condition end
+
+# addition start
+# x = 10
+# y = 12
+# total = x + y
+# print("Total:",total)
+# addition end
+
+# int start
+# myint = 7
+# print(myint)
+# int end
+
+# float start
+# myfloat = 7.3
+# myfloat2 = float(8)
+# print(myfloat2)
+# float end
+
+# string start
+# mystring = 'This is String'
+# mystring2 = "This is also String" #double quotes can use single quote in the string (')
+# print(mystring)
+# print(mystring2)
+# string end
+
+# simple operator start 
+# one = 1
+# two = 2
+# three = one + two
+# print(three)
+#
+# hello = "Hello"
+# world = "World"
+# helloworld = hello + ", " + world + "!"
+# print(helloworld)
+# simple operator end 
+
+# assignments start
+# a,b = 1,2
+# print(a,b)
+# assignments end
---
+++ 6_conditions.py
@@ -0,0 +1,43 @@
+# Boolean start
+# x = 2
+# print(x == 2) # True
+# print(x == 3) # False
+# print(x < 3)  # True
+# Boolean end
+
+# Boolean operators start
+# name = "Aditya"
+# age = 23
+# if name == "Aditya" and age == 23:
+#     print(f"Your name is {name}, and you are also {age} years old.")
+# if name == "Aditya" or name == "Wiranata":
+#     print("Your name is either Aditya or Wiranata.")
+# Boolean operators end
+
+# in operator start
+# name = "Aditya"
+# if name in ["Aditya", "Wiranata"]:
+#     print("Your name is either Aditya or Wiranata.")
+# in operator end
+
+# if using code blocks start
+# statement = False
+# another_statement = True
+# if statement is True:
+#     print("Statement is True")
+#     pass
+# elif another_statement is True:
+#     print("Another statement is True:")
+#     pass
+# else:
+#     print("The statements is False")
+#     pass
+# if using code blocks start
+
+# if else test start
+# x = 2
+# if x==2:
+#     print("x equals two!")
+# else:
+#     print("x does not equal to two.")
+# if else test end
